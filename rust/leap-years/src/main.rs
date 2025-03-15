4d3b391b98cc103a00104496218fb347fd775cd5
Joe Sharp <ratracejoe@hotmail.com>
2025-03-15 19:17:09 +0000

    leap years working

--- rust/leap-years/src/main.rs
+++ rust/leap-years/src/main.rs
@@ -1,6 +1,14 @@
-fn is_leap_year(year: u32) -> bool {
-    let div_4 = year % 4 == 0
+fn is_div(value: u32, divisor: u32) -> bool {
+    value % divisor == 0
+}
 
+fn is_leap_year(year: u32) -> bool {
+    let d4 = is_div(year, 4);
+    let d100 = is_div(year, 100);
+    let d400 = is_div(year, 400);
+
+    if d400 { return true; }
+    if !d100 && d4 { return true; }
     false
 }
 
@@ -13,6 +21,13 @@ fn main() {
 mod tests {
 
     use super::*;
+
+    #[test]
+    fn test_is_div() {
+        assert!(is_div(12, 3));
+        assert!(is_div(30, 5));
+        assert!(!is_div(11, 3));
+    }
 
     #[test]
     fn test_leap_year() {
