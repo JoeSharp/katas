be366136abf5c3a5d03121435c5f79392ded0fc7
Joe Sharp <ratracejoe@hotmail.com>
2025-03-21 18:56:39 +0000

    closest to zero

--- rust/closest-to-zero/src/main.rs
+++ rust/closest-to-zero/src/main.rs
@@ -3,7 +3,17 @@ fn main() {
 }
 
 fn closest_to_zero(numbers: Vec<u32>) -> u32 {
-    numbers[0]
+    if numbers.len() == 0 {
+        return 0
+    }
+    let mut min: u32 = numbers[0];
+    for n in numbers {
+        if n < min {
+            min = n
+        }
+    }
+
+    min
 }
 
 #[cfg(test)]
@@ -12,6 +22,7 @@ mod tests {
 
     #[test]
     fn finds_closest_to_zero() {
+        assert_eq!(closest_to_zero(vec![]), 0);
         assert_eq!(closest_to_zero(vec![1, 2, 3, 4]), 1);
         assert_eq!(closest_to_zero(vec![4, 3, 1, 2]), 1);
     }
