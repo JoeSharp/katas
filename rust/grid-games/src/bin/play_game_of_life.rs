888e9bd20bacdd0f44eda4959e2867fc0b4b7613
Joe Sharp <ratracejoe@hotmail.com>
2025-04-15 08:02:51 +0100

    refactoring along with the rust book

--- rust/grid-games/src/bin/play_game_of_life.rs
+++ rust/grid-games/src/bin/play_game_of_life.rs
@@ -1,24 +1,35 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 use grid_games::game_of_life::GameOfLife;
 
 use std::{io, thread, time::Duration};
 
 const FRAME_TIME: u64 = 100;
-const FRAME_COUNT: u64 = 100;
+const FRAME_COUNT: u64 = 10;
 
-fn main() -> Result<(), io::Error> {
-    let args: Vec<String> = env::args().collect();
+struct Config<'a> {
+    basefile: &'a str,
+}
 
-    if args.len() < 2 {
-        panic!("Not enough arguments, you need to pass a filename");
+impl<'a> Config<'a> {
+    fn new(args: &'a [String]) -> Result<Config<'a>, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments, you need to pass a filename");
+        }
+
+        let basefile = &args[1];
+        Ok(Config { basefile })
     }
+}
 
-    let basefile = &args[1];
-    println!("Game of Life - Example {}", basefile);
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("Game of Life - Example {}", config.basefile);
 
-    let contents = fs::read_to_string(basefile).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.basefile).expect("Should have been able to read the file");
 
     let mut board: GameOfLife = GameOfLife::from_str(&contents).unwrap();
 
@@ -28,6 +39,22 @@ fn main() -> Result<(), io::Error> {
 
         board.iterate();
         thread::sleep(Duration::from_millis(FRAME_TIME));
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
     }
 
     Ok(())
